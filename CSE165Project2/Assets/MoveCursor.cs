0f8f779ae03271c3dfa16ff9657ddceddec6a743
mackenziemonroe <mmonroe@UCSD.EDU>
2018-02-07 12:42:22 -0800

    Lots of Stuff

--- CSE165Project2/Assets/MoveCursor.cs
+++ CSE165Project2/Assets/MoveCursor.cs
@@ -16,9 +16,9 @@ public class MoveCursor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Vector3 offset = new Vector3(0,0.15f,0);
         //transform.position = cam.transform.position + cam.transform.forward * 8;
-        transform.position = cam.transform.position;
+        transform.position = cam.transform.position + offset;
 
         transform.rotation = cam.transform.rotation;
 
---
+++ CSE165Project2/Assets/ResetRot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetRot : MonoBehaviour {
+
+    private Vector3 prevPos;
+
+    private float initialRotX;
+    private float initialRotZ;
+
+    // Use this for initialization
+    void Start () {
+
+
+        prevPos = transform.position;
+
+        initialRotX = transform.rotation.x;
+        initialRotZ = transform.rotation.z;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (prevPos == transform.position)
+        {
+            Quaternion temp = new Quaternion(initialRotX,transform.rotation.y, initialRotZ, 1.0f);
+            //reset x and z orientation
+            //transform.rotation.x = initialRotX;
+            //transform.rotation.z = initialRotZ;
+            transform.rotation = temp;
+
+
+
+        } else
+        {
+
+            prevPos = transform.position;
+
+
+        }
+		
+	}
+}
